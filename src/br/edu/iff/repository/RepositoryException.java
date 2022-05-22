495a1a47315ab52d04f084cfa33a3753135d76e0
Yves de Sá <yvesdsb98@gmail.com>
2022-05-22 14:42:27 -0300

    feat: repository interface and repository exception created

---
+++ src/br/edu/iff/repository/RepositoryException.java
@@ -0,0 +1,9 @@
+package br.edu.iff.repository;
+
+public class RepositoryException extends Exception {
+  private static final long serialVersionUID = 1L;
+
+  public RepositoryException(String message) {
+    super(message);
+  }
+}
