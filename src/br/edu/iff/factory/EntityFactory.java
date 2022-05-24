914a07c9754d99b84c71f1c9884301ec1d3b4f6b
Yves de Sá <yvesdsb98@gmail.com>
2022-05-24 04:23:03 -0300

    feat: created the entity factory

---
+++ src/br/edu/iff/factory/EntityFactory.java
@@ -0,0 +1,19 @@
+package br.edu.iff.factory;
+
+import br.edu.iff.repository.Repository;
+
+public abstract class EntityFactory {
+  private Repository repository;
+
+  protected EntityFactory(Repository repository) {
+    this.repository = repository;
+  }
+
+  protected Repository getRepository() {
+    return repository;
+  }
+
+  protected Long getNextId() {
+    return repository.getNextId();
+  }
+}
